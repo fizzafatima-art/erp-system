389b06b8f014dc7d65eca917a02f58601dd86d1b
FIZZA FATIMA <fizza.abbaspk@gmail.com>
2026-06-11 04:04:47 +0500

    fix: remove undefined returnPurchase from purchaseRoutes

--- src/routes/purchaseRoutes.js
+++ src/routes/purchaseRoutes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const purchaseController = require('../controllers/purchaseController');
 
+router.get('/',            purchaseController.getAllPurchases);
+router.post('/',           purchaseController.createPurchase);
+router.get('/:id',         purchaseController.getPurchaseById);
 router.put('/:id/payment', purchaseController.addPayment);
-router.get('/', purchaseController.getAllPurchases);
-router.post('/', purchaseController.createPurchase);
-router.get('/:id', purchaseController.getPurchaseById);
-router.post('/return', purchaseController.returnPurchase); // <--- ADDED
-router.post('/:id/payment', purchaseController.addPayment);
 
 module.exports = router;
