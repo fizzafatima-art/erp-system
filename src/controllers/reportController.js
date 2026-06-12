1dddf93c844b66933fb249374b6dd1500dad00e9
FIZZA FATIMA <fizza.abbaspk@gmail.com>
2026-06-13 02:51:27 +0500

    new

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,4 +1,4 @@
-const db = require('../config/database');
+const db = require('../../config/database'); // Do steps up (../) root jane ke liye
 
 // ==========================================
 // DASHBOARD KPIs
